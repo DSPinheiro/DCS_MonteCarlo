0ca7fee4f2e9ce7299bf5619a45ad8e7a0267a10
Daniel Pinheiro <ds.pinheiro@campus.fct.unl.pt>
2023-06-28 00:40:54 +0200

    Cleanup of the merge shitshow

--- source_complex.cpp
+++ source_complex.cpp
@@ -928,48 +928,6 @@ bool Source_complex::run_Source(SimulationInterface *w){
                                             }
                                             #endif
                                         }
-                                        else {
-                                            if (GraphOptionsInput.make_image_plates) {
-                                                //If the event does not reach the detector then only add when we have less than maxEventNum
-                                                if (eventsToTrace_para.size() < NumberRaysInput.number_events && tmpEvent.size() >= 6) {
-                                                    eventsToTrace_para.push_back(tmpEvent);
-                                                }
-                                            }
-
-                                        }
-
-                                    }
-                                    else {
-                                        if (GraphOptionsInput.make_image_plates) {
-                                            //If the event does not reach the detector then only add when we have less than maxEventNum
-                                            #ifdef OPENMP
-                                            #pragma omp critical
-                                            {
-                                            #endif
-                                            if (eventsToTrace_para.size() < NumberRaysInput.number_events && tmpEvent.size() >= 6) {
-                                                eventsToTrace_para.push_back(tmpEvent);
-                                            }
-                                            #ifdef OPENMP
-                                            }
-                                            #endif
-                                        }
-
-                                    }
-
-                                }
-                                else {
-                                    if (GraphOptionsInput.make_image_plates) {
-                                        //If the event does not reach the detector then only add when we have less than maxEventNum
-                                        #ifdef OPENMP
-                                        #pragma omp critical
-                                        {
-                                        #endif
-                                        if (eventsToTrace_para.size() < NumberRaysInput.number_events && tmpEvent.size() >= 6) {
-                                            eventsToTrace_para.push_back(tmpEvent);
-                                        }
-                                        #ifdef OPENMP
-                                        }
-                                        #endif
                                     }
                                 }
                                 else {
@@ -987,9 +945,6 @@ bool Source_complex::run_Source(SimulationInterface *w){
                                         #endif
                                     }
                                 }
-
-
-
                             }
                             else {
                                 if (GraphOptionsInput.make_image_plates) {
@@ -1006,8 +961,6 @@ bool Source_complex::run_Source(SimulationInterface *w){
                                     #endif
                                 }
                             }
-
-
 
                         }
 
@@ -1189,21 +1142,6 @@ bool Source_complex::run_Source(SimulationInterface *w){
                                             #endif
                                         }
                                     }
-                                    else {
-                                        if (GraphOptionsInput.make_image_plates) {
-                                            //If the event does not reach the detector then only add when we have less than maxEventNum
-                                            #ifdef OPENMP
-                                            #pragma omp critical
-                                            {
-                                            #endif
-                                            if (eventsToTrace_anti.size() < NumberRaysInput.number_events && tmpEvent.size() >= 6) {
-                                                eventsToTrace_anti.push_back(tmpEvent);
-                                            }
-                                            #ifdef OPENMP
-                                            }
-                                            #endif
-                                        }
-                                    }
 
                                 }
                                 else {
@@ -1279,44 +1217,6 @@ bool Source_complex::run_Source(SimulationInterface *w){
                 return false;
             }
 
-            #ifdef OPENMP
-            #pragma omp atomic
-            #endif
-            I++;
-
-            #ifdef QT_EXISTS
-                if (!w->isOpen())
-                {
-                    gener_out << "********************************" << endl;
-                    gener_out << endl;
-                    gener_out << " SIMULATION TERMINATED BEFORE FINISH!!!" << endl;
-                    gener_out << endl;
-                    gener_out << " RESULTS ARE GARBAGE!!!" << endl;
-                    gener_out << endl;
-                    gener_out << "********************************" << endl;
-                    gener_out << endl;
-
-                    w->setPctDone(1.0f);
-
-                    return false;
-                }
-
-            if(GraphOptionsInput.make_image_plates){
-            emit w->changeStats(
-                SimulationInterface::Stats
-                {
-                    counts_sour,
-                    counts_C1,
-                    counts_C2_para,
-                    counts_C2_anti,
-                    counts_detc_para,
-                    counts_detc_anti,
-                    delrot,
-                    eventsToTrace_para,
-                    eventsToTrace_anti
-                }
-            );
-            }
 
             if(GraphOptionsInput.make_image_plates){
             emit w->changeStatsSignal(
